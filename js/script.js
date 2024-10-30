9e7e19d80db17762f5f8965aae8b880f302e6f6d
ayrton <ayrtonalexanderupp@gmail.com>
2024-10-30 16:18:37 -0300

    version 1.0.7

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const images = [
     'images/carousel-images/calabazas.jpg',
     'images/carousel-images/dog.jpg',
-    'images/carousel-images/hallowen.jpg'
+    '../images/carousel-images/hallowen.jpg'
 ];
 
 let currentIndex = 0;
@@ -44,6 +44,15 @@ document.querySelectorAll('.dropdown-content a').forEach(link => {
 });
 
 
+document.querySelectorAll('.section-container section p').forEach((p) => {
+    p.addEventListener('mouseenter', () => {
+        p.style.transform = 'scale(1.05)';
+        p.style.transition = 'transform 0.3s ease';
+    });
+    p.addEventListener('mouseleave', () => {
+        p.style.transform = 'scale(1)';
+    });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     const cookieBanner = document.getElementById("cookie-banner");
